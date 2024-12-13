d786ac6c2ae2deea0c3eeae741c773eb54b606a6
nicholas harper <nicholasharper000@gmail.com>
2024-12-12 17:19:16 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,4 @@
-/**
- * Index file.
- *
- * @package react-app-boilerplate
- */
-
-/**
- * External dependency
- */
 import ReactDOM from 'react-dom';
-
-/**
- * Internal dependecy
- */
 import App from './App';
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
